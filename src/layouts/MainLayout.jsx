802eecd0635e6e6ea3e588d290621b1206f39153
tuRjoX <tdas41380@gmail.com>
2026-02-02 20:41:24 +0600

    feat: Create MainLayout component with Navbar and Footer structure

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
